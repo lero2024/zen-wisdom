ab498ad285b1b63258bc6a198c447528ee8343ec
OneDay AI 代码助手 <oneday_bot@alibaba-inc.com>
2025-03-16 14:10:47 +0800

    Update webpack publicPath for better asset loading

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
-    publicPath: './',
+    publicPath: '/',
     clean: true
   },
   optimization: {
